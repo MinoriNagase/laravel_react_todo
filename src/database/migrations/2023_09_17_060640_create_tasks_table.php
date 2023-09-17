6e82dd57b99e041b8b71d75dc4ad5c61f4e5d27c
MinoriNagase <minori.nagase@arsaga.jp>
2023-09-17 15:26:20 +0900

    Add: tasksテーブル、Taskモデル、ダミーデータ追加

---
+++ src/database/migrations/2023_09_17_060640_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('content', '100')->comment('タスクの内容');
+            $table->tinyInteger('status')->default(0)->comment('タスクの状態 0:未完了 1:完了');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
