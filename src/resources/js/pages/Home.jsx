273fa7d277ce428a6a0c623883483bb39b99f4b3
MinoriNagase <minori.nagase@arsaga.jp>
2023-10-01 11:00:05 +0900

    Add: 仮の一覧表示ビュー

--- src/resources/js/pages/Home.jsx
+++ src/resources/js/pages/Home.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 function Home() {
+
+    // タスク内容の配列
+    const tasks = [
+        {content: '洗濯物を干す'},
+        {content: '掃除機をかける'},
+    ]
+
     return (
-        <div>
-            <h1>Home Component</h1>
-            <p>I'm an home component!</p>
-            <Link to={'/page'}>Pageへ遷移</Link>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-10">
+                    <h1>タスク管理</h1>
+                    <table className="table">
+                        {/* タスク表示部分 */}
+                        <tbody>
+                            {tasks.map((task, index) => (
+                                <tr key={index}>
+                                    <td className="text-center">{task.content}</td>
+                                    <td className="text-center">
+                                        <button className="btn btn-primary">完了</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <Link to={'/page'}>Pageへ遷移</Link>
+                </div>
+            </div>
         </div>
     );
 }
