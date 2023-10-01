fd546657d7110edc56c855acd274d695c1efe9d3
MinoriNagase <minori.nagase@arsaga.jp>
2023-10-01 12:53:02 +0900

    Add: タスク完了APIとの繋ぎこみ

--- src/resources/js/components/Table.jsx
+++ src/resources/js/components/Table.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 function Table(props) {
-    const {rows} = props;
+    const { rows, completeTask } = props;
     return (
         <table className="table">
             {/* タスク表示部分 */}
             <tbody>
-            {rows.map((row, index) => (<tr key={index}>
-                    <td className="text-center">{row.content}</td>
-                    <td className="text-center">
-                        <button className="btn btn-primary">完了</button>
-                    </td>
-                </tr>))}
+                {rows.map((row, index) =>
+                    (<tr key={index}>
+                        <td className="text-center">{row.content}</td>
+                        <td className="text-center">
+                            <button className="btn btn-primary" onClick={() => completeTask(row.id)}>完了</button>
+                        </td>
+                    </tr>))}
             </tbody>
         </table>
     );
--- src/resources/js/pages/Home.jsx
+++ src/resources/js/pages/Home.jsx
@@ -25,10 +25,25 @@ function Home() {
             });
     }
 
+    const completeTask = (taskId) => {
+        axios
+            .patch(`/api/tasks/${taskId}/complete`)
+            .then(response => {
+                console.log('タスクが完了しました', response.data);
+                getTasksData()
+            })
+            .catch(error => {
+                console.error('タスク完了できませんでした', error);
+            });
+    }
+
     // tasksの要素ごとにrowsで扱える形式にする
     const rows = tasks.map((task) => ({
+        id: task.id,
         content: task.content,
-        completeButton: <button color="primary">完了</button>,
+        completeButton: (
+            <button color="primary" onClick={() => completeTask(task.id)}>完了</button>
+        )
     }));
 
     return (
@@ -36,7 +51,7 @@ function Home() {
             <div className="row justify-content-center">
                 <div className="col-md-10">
                     <h1>未完了タスク一覧</h1>
-                    <Table rows={rows}/>
+                    <Table rows={rows} completeTask={completeTask}/>
                     <Link to={'/page'}>Pageへ遷移</Link>
                 </div>
             </div>
